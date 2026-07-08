79b27b513d69d58ac0c6b38c824aa714c46cbd9d
vivek <vivek33123@gmail.com>
2026-07-09 02:21:14 +0530

    link update

--- config/resources.js
+++ config/resources.js
@@ -4,7 +4,7 @@ module.exports = {
         fix: "https://drive.google.com/file/d/1gbBavelChT9BWbpyngP7md4vVnIVVUOr/view?usp=sharing",
 
         tutorial: {
-            channelId: 3943904498,
+            channelId: -1003943904498,
             messageId: 3
         }
     }
